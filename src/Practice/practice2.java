26aa93af0ff4186f16190621dc65e96634fbe1b1
sakurai <k-sakurai@jellyfish-g.co.jp>
2018-10-31 15:43:37 +0900

    First commit

--- src/Practice/practice2.java
+++ src/Practice/practice2.java
@@ -27,28 +27,99 @@ public class practice2 {
 			Document doc = Jsoup.connect(url).get();
 
 			if (url.equals("http://books.toscrape.com/")) {
-				Element element = doc.selectFirst("#default > div > div > div > div > form > strong:nth-child(2)");
+				Element element = doc.selectFirst("#default > div > div > div > div > form > strong:nth-child(2)"); //All products 1000 results
+
 				String text = element.text();
 				int pages = Integer.parseInt(text);
 				int page_num = pages / 20;
 				for (int j = 1; j <= page_num; j++) {
-					String temp = String.format("http://books.toscrape.com/catalogue/page-%d.html", j);
+					String temp = String.format("http://books.toscrape.com/catalogue/page-%d.html", j);//%d = integer of the page_num
 					urlList.add(temp);
+					for (String urlFixed : urlList) {
+						Document doc2 = Jsoup.connect(urlFixed).get();
+						Elements titles = doc.select(
+								"#default > div > div > div > div > section > div:nth-child(2) > ol > li> article > h3 > a");
+						for (Element title : titles) {
+
+							System.out.println(title.text());
+
+						}
+						String selectorPage = "#default > div > div > div > div > section > div > ol > li > article > h3 > a";
+
+						Elements elements = doc.select(selectorPage);
+						for (Element elementBooks : elements) {
+							String textUrl = elementBooks.absUrl("href");
+							Document doc3 = Jsoup.connect(textUrl).get();
+
+							Elements UPC = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(1) > td");
+							Elements Product_Type = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(2) > td");
+							Elements Price_excl_tax = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(3) > td");
+							Elements Price_incl_tax = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(4) > td");
+							Elements Tax = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(5) > td");
+							Elements Availability = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(6) > td");
+							Elements Number_of_reviews = doc3
+									.select("#content_inner > article > table > tbody > tr:nth-child(7) > td");
+
+							System.out.println(UPC.text());
+							System.out.println(Product_Type.text());
+							System.out.println(Price_excl_tax.text());
+							System.out.println(Price_incl_tax.text());
+							System.out.println(Tax.text());
+							System.out.println(Availability.text());
+							System.out.println(Number_of_reviews.text());
+
+						}
+					}
 				}
+//			} else if (url.contains("catalogue/page"))
+//
+//			{
+//				Elements titles = doc.select(
+//						"#default > div > div > div > div > section > div:nth-child(2) > ol > li> article > h3 > a");
+//				for (Element title : titles) {
+//
+//					System.out.println(title.text());
+//
+//				}
 
-			} else if (url.contains("catalogue/page"))
-
-			{
-				Elements titles = doc.select(
-						"#default > div > div > div > div > section > div:nth-child(2) > ol > li> article > h3 > a");
-				for (Element title : titles) {
-
-					System.out.println(title.text());
-
-				}
 			}
+			//				else if (url.contains("catalogue/page")) {
+
+			//				Elements titles = doc.select(
+			//						"#default > div > div > div > div > section > div:nth-child(2) > ol > li> article > h3 > a");
+			//				for (Element title : titles) {
+			//					Document doc2 = Jsoup.connect(title.attr("href:abs")).get();
+			//
+			//					Elements UPC = doc2.select("#content_inner > article > table > tbody > tr:nth-child(1) > td");
+			//					Elements Product_Type = doc2
+			//							.select("#content_inner > article > table > tbody > tr:nth-child(2) > td");
+			//					Elements Price_excl_tax = doc2
+			//							.select("#content_inner > article > table > tbody > tr:nth-child(3) > td");
+			//					Elements Price_incl_tax = doc2
+			//							.select("#content_inner > article > table > tbody > tr:nth-child(4) > td");
+			//					Elements Tax = doc2.select("#content_inner > article > table > tbody > tr:nth-child(5) > td");
+			//					Elements Availability = doc2
+			//							.select("#content_inner > article > table > tbody > tr:nth-child(6) > td");
+			//					Elements Number_of_reviews = doc2
+			//							.select("#content_inner > article > table > tbody > tr:nth-child(7) > td");
+			//
+			//					System.out.println(UPC.text());
+			//					System.out.println(Product_Type.text());
+			//					System.out.println(Price_excl_tax.text());
+			//					System.out.println(Price_incl_tax.text());
+			//					System.out.println(Tax.text());
+			//					System.out.println(Availability.text());
+			//					System.out.println(Number_of_reviews.text());
+			//
+			//				}
+			//			}
 		}
 
 	}
-
 }
